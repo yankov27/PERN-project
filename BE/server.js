2e096d4eb859f936fb381157fd37dd9ff1c0ed99
Daniel Yankov <yankov27@gmail.com>
2023-05-09 08:40:24 +0300

    React progress

--- BE/server.js
+++ BE/server.js
@@ -6,40 +6,102 @@ const app = express();
 
 //middleware
 app.use(express.json());
-// app.use(morgan("tiny"));
-// app.use((req, res, next) => {
-//   console.log("middleware");
-//   next();
-// });
 
 //get all restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
-  const results = await db.query("SELECT * FROM restaurants");
-  console.log(results);
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurants: ["McDonalds", "Wendys"],
-    },
-  });
+  try {
+    const results = await db.query("SELECT * FROM restaurants");
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //get a restaurant
-app.get("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params);
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const results = await db.query("SELECT * FROM restaurants WHERE id = $1", [
+      id,
+    ]);
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //create a restaurant
-app.post("/api/v1/restaurants/", (req, res) => {});
+app.post("/api/v1/restaurants/", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const location = req.body.location;
+    const price_range = req.body.price_range;
+    const results = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) RETURNING *",
+      [name, location, price_range]
+    );
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 //update a restaurant
-app.put("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params.id);
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const name = req.body.name;
+    const location = req.body.location;
+    const price_range = req.body.price_range;
+    const results = await db.query(
+      "UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *",
+      [name, location, price_range, id]
+    );
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //delete a restaurant
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params.id);
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const results = await db.query("DELETE FROM restaurants WHERE id = $1", [
+      id,
+    ]);
+    res.status(204).json({
+      status: "success",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const port = process.env.PORT || 3006;
---
+++ fe/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/restaurants/:id/update" Component={UpdatePage} />
+          <Route
+            exact
+            path="/restaurants/:id"
+            Component={RestaurantDetailPage}
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
