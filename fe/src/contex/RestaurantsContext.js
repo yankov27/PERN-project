f65f70c890ff23b8d738cb97a73779e22f7dc87d
Daniel Yankov <yankov27@gmail.com>
2023-05-21 20:51:49 +0300

    littleProggress

--- fe/src/contex/RestaurantsContext.js
+++ fe/src/contex/RestaurantsContext.js
@@ -3,6 +3,7 @@ import React, { useState, createContext } from "react";
 export const RestaurantsContext = createContext();
 export const RestaurantsContextProvider = (props) => {
   const [restaurants, setRestaurants] = useState([]);
+  const [selectedRestaurant, setSelectedRestaurant] = useState([]);
 
   const addRestaurant = (restaurant) => {
     setRestaurants([...restaurants, restaurant]);
@@ -10,7 +11,13 @@ export const RestaurantsContextProvider = (props) => {
 
   return (
     <RestaurantsContext.Provider
-      value={{ restaurants, setRestaurants, addRestaurant }}
+      value={{
+        restaurants,
+        setRestaurants,
+        addRestaurant,
+        selectedRestaurant,
+        setSelectedRestaurant,
+      }}
     >
       {props.children}
     </RestaurantsContext.Provider>
